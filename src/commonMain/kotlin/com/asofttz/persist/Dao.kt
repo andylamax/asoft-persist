093b5fb7256b338c2d82833fa14cd58707d2b22a
Anderson Lameck <andylamax@programmer.net>
2019-03-28 00:13:37 +0300

    march-backup

--- src/commonMain/kotlin/com/asofttz/persist/Dao.kt
+++ src/commonMain/kotlin/com/asofttz/persist/Dao.kt
@@ -5,10 +5,10 @@ import com.asofttz.rx.ObservableList
 abstract class Dao<T> {
     protected val cached = ObservableList<T>()
 
-    suspend fun filter(predicate: (T)->Boolean) = cached.value.filter(predicate)
-    abstract suspend fun create(t: T): Boolean
-    abstract suspend fun edit(t: T): Boolean
-    abstract suspend fun delete(t: T): Boolean
-    abstract suspend fun load(id: Int): T?
-    abstract suspend fun loadAll(): ObservableList<T>
+    open suspend fun filter(predicate: (T) -> Boolean) = cached.value.filter(predicate)
+    open suspend fun create(t: T): Boolean = cached.add(t)
+    open suspend fun edit(t: T): Boolean = true
+    open suspend fun delete(t: T): Boolean = cached.remove(t)
+    open suspend fun load(id: Int): T? = cached.value.getOrNull(id)
+    open suspend fun loadAll(): ObservableList<T> = cached
 }
