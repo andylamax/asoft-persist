dabe8eda8a7058ce53895ceb670563815bbf7a4f
Anderson Lameck <andylamax@programmer.net>
2019-05-16 00:04:24 +0300

    Added the singleton class

---
+++ src/commonMain/kotlin/com/asofttz/persist/Singleton.kt
@@ -0,0 +1,6 @@
+package com.asofttz.persist
+
+abstract class Singleton<in In, out Out>(private val builder: (In) -> Out) {
+    private var instance: Out? = null
+    fun getInstance(config: In) = instance ?: builder(config).also { instance = it }
+}
