800a208a03bd6cfcdd0f0eea4c7741307ced17d2
Anderson Lameck <andylamax@programmer.net>
2019-07-14 19:00:46 +0300

    Added the serializable result tool

---
+++ src/commonMain/kotlin/tz/co/asoft/persist/result/Result.kt
@@ -0,0 +1,32 @@
+package tz.co.asoft.persist.result
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
+import tz.co.asoft.persist.tools.Cause
+
+@Serializable
+class Result<out T>(val data: T? = null, val error: String? = null) {
+    var status = error == null
+
+    @Transient
+    var cause: Cause? = null
+
+    companion object {
+        fun <T> success(data: T) = Result(data)
+
+        fun <T> failure(cause: Cause) = Result<T>(null, cause.message)
+
+        suspend fun <T> catching(script: suspend () -> T?): Result<T> {
+            var t: T? = null
+            var cause: Cause? = null
+            try {
+                t = script()
+            } catch (c: Cause) {
+                cause = c
+            }
+            return Result(t, cause?.message).also {
+                it.cause = cause
+            }
+        }
+    }
+}
---
+++ src/commonMain/kotlin/tz/co/asoft/persist/tools/Throwable.kt
@@ -0,0 +1,3 @@
+package tz.co.asoft.persist.tools
+
+typealias Cause = Throwable
